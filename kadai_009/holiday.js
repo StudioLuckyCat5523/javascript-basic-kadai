e6b8c850359b73cdcca3f8952151d3fb5cd5fb95
StudioLuckyCat5523 <162400926+StudioLuckyCat5523@users.noreply.github.com>
2024-08-26 08:41:42 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+
+//祝日一覧変数に代入
+let holidayNames = ['正月','成人の日','建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日','スポーツの日','文化の日','勤労感謝の日'];
+
+//変数holidayNamesを出力し続ける
+for(let i=0; i < holidayNames.length; i++){
+	console.log(holidayNames[i]);
+}
+
+//whileを使う
+const holidayName= ['正月','成人の日','建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日','スポーツの日','文化の日','勤労感謝の日'];
+  
+
+let i = 0;
+
+//変数holidayNameが0以外である間、値を出力しつづける
+while (i < holidayName.length){
+	console.log(holidayName[i]);
+	i++;
+}
+	
+	
