d20e191daa9f06850301409b59d43de12bdea53f
StudioLuckyCat5523 <162400926+StudioLuckyCat5523@users.noreply.github.com>
2024-08-30 14:12:16 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+//本日の日付
+const today = new Date();
+//何年
+const year = today.getFullYear();
+
+//何月
+
+const month = today.getMonth()+1;
+
+//何日
+
+const date = today.
+getDate();
+
+
+//今日は
+console.log(today.getFullYear()+'年'+ (today.getMonth()+1)+'月'+ today.
+getDate()+'日');
