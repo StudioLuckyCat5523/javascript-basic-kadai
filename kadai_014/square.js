b031c4e5df0ea789dd5bba2138827c3f7d5025ca
StudioLuckyCat5523 <162400926+StudioLuckyCat5523@users.noreply.github.com>
2024-09-13 14:05:09 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+//引数nemを2乗してその値を出力する
+const userSquare = (number) => {
+	//数値を二乗して戻り値を返す
+	return number*number;
+ 
+};
+
+//関数を使う
+
+const result = userSquare(10);
+//結果
+
+console.log(result);
+
